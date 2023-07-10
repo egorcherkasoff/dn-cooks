391e219ca62d34d84191b8ba699546392f64543b
Егор Черкасов <egor-cherkasoff.ps@yandex.ru>
2023-07-10 22:27:25 +0800

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+    <header class="py-5 bg-red-400 text-white">
+        <nav class="flex justify-between container mx-auto items-center">
+            <NuxtLink to="/" class="text-xl font-bold">DN Cooks</NuxtLink>
+            <ul class="flex text-lg font-semibold">
+                <li>
+                    <NuxtLink
+                        to="/"
+                        class="border-b-2 border-red-400 transition-all ease-out hover:border-white"
+                        >Recipes</NuxtLink
+                    >
+                </li>
+                <li>
+                    <NuxtLink
+                        to="/"
+                        class="ms-10 border-b-2 border-red-400 transition-all ease-out hover:border-white"
+                        >Login</NuxtLink
+                    >
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <slot></slot>
+    </main>
+
+    <footer class="py-5 bg-red-700 text-white">
+        <nav class="container mx-auto flex justify-between items-center">
+            <div>
+                <NuxtLink to="/" class="text-xl font-bold">DN Cooks</NuxtLink>
+                <p class="text-xs">
+                    by
+                    <a href="https://github.com/egorcherkasoff" target="_blank"
+                        >@egorcherkasoff</a
+                    >
+                </p>
+            </div>
+            <ul class="flex text-lg font-semibold">
+                <li>
+                    <NuxtLink
+                        to="/"
+                        class="border-b-2 border-red-700 transition-all ease-out hover:border-white"
+                        >Recipes</NuxtLink
+                    >
+                </li>
+                <li>
+                    <NuxtLink
+                        to="/"
+                        class="ms-10 border-b-2 border-red-700 transition-all ease-out hover:border-white"
+                        >Login</NuxtLink
+                    >
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</template>
+
+<script></script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="text-center bg-red-400 py-28 text-white">
+        <h1 class="text-4xl font-bold tracking-wider">DN Cooks</h1>
+        <p class="text-lg tracking-wide font-semibold mb-4">
+            For all your cooking needs
+        </p>
+        <NuxtLink
+            to="/"
+            class="tracking-widest border-b-2 border-red-400 transition-all ease-out hover:border-white"
+            >discover recipes</NuxtLink
+        >
+    </section>
+    <section class="container mx-auto"></section>
+</template>
